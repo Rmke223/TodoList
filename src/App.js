2aea69b40a103cb49d56f5cd7224304e6fbda0ab
Rmke223 <70180189+Rmke223@users.noreply.github.com>
2020-10-20 12:05:38 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userInput: "",
+      list: [],
+      id: 0,
+      isDone: null
+    }
+  }
+
+  changeUserInput(input) {
+    this.setState({
+      userInput: input
+    }, () => console.log(input)
+    )
+  }
+  addToList(input) {
+    let listArray = this.state.list;
+    listArray.push(input);
+    this.setState({
+      list: listArray,
+      userInput: '',
+    })
+  }
+  removeFromList(id) {
+    let idArray = this.state.list;
+    idArray.splice(id, 1);
+    this.setState({
+      list: idArray
+    })
+  }
+
+ isClicked(){
+   let icon = this.state.isDone
+   if(icon == null){
+      icon = <i class="fas fa-check"></i>
+      this.setState({
+        isDone: icon
+      })
+   }
+   else{
+     this.setState({
+       isDone: null
+     })
+   }
+ }
+
+
+  render() {
+    return (
+      <div className="text-center bg-success text-white">
+        <h1>To-Do List</h1>
+        <input
+          onChange={(e) => this.changeUserInput(e.target.value)}
+          type="text"
+          value={this.state.userInput}
+          placeholder="Add Task"
+        />
+        <button className="btn btn-primary" onClick={() => this.addToList(this.state.userInput)}><b>Add</b></button>
+        <ul className="list-group">
+          {this.state.list.map((val, i) =>
+            <div
+              key={val}
+              id={this.state.list.indexOf(val)}
+              className="list-group-item d-flex justify-content-between text-primary">
+             <div className="col-1 border text-center"
+              onClick={(e) => this.isClicked(e.target.value)} >
+             {this.state.isDone}
+             </div>
+              {/* <input onChange={(e) => this.checkbox(e.target)} 
+              type="checkbox" 
+              className="form-check-input mt-2"></input> */}
+              <h4>{val}</h4>
+              <button
+                onClick={() => this.removeFromList(i)}
+                className="btn btn-danger text-white">
+                <i className="far fa-trash-alt"></i>
+              </button>
+            </div>)}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
